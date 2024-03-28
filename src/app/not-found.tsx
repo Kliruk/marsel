bc50676f2b98973a6ebe76a71ef95f6c50222c12
Kliruk <mazurenko.corporate@gmail.com>
2024-03-28 12:51:06 +0200

    Зробив сторінку not-found та адаптацію для неї

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,31 @@
 import {Navbar} from "@/components";
+import CustomHeader from "@/hooks/CustomHeader";
+import Image from "next/image";
+import styles from "@/styles/root/global-not-found.module.css";
+import ButtonSquared from "@/UI's/buttons/buttons-links/ButtonSquared";
 
 /**
  * Global page 404, that will appear when it is unknown route
- * @return {JSX}
+ * @return {JSX.Element}
  */
-export default function NotFound() {
+export default function NotFound(): JSX.Element {
   return (
     <div>
       <Navbar />
+      <div className={styles.main}>
+        {/* ФОТО - ТЕСТОВІ */}
+        <Image className={styles.leftPhoto} src={"/photo/test.jpg"}
+          width={281} height={438} alt="Помилка" />
+        <div className={styles.textAndbutton}>
+          <CustomHeader headerType={"h2"} isYmoves={true} >
+            ЩОСЬ ПIШЛО НЕ ТАК
+          </CustomHeader>
+          <ButtonSquared text={"повернутися назад"} link={"-1"} type={"none"} widthStar={12} />
+        </div>
+        {/* ФОТО - ТЕСТОВІ */}
+        <Image className={styles.rightPhoto} src={"/photo/test.jpg"}
+          width={281} height={438} alt="Помилка" />
+      </div>
     </div>
   );
 }
