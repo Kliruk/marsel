483d24df556085cf4019e8864c374f89b98662b8
Kliruk <mazurenko.corporate@gmail.com>
2024-04-01 22:13:49 +0300

    New feauture: CustomHeader можливість вказувати колір для заголовку та дефолтне значення кольору --color-black

--- src/hooks/CustomHeader.tsx
+++ src/hooks/CustomHeader.tsx
@@ -18,8 +18,8 @@ import {HOOKS_DELAY_DEFAULT, HOOKS_DURATION_DEFAULT} from "@/constants/constants
  * @param {delay} delay of the animation, provided with default value
  * @return {JSX.Element}
  */
-const CustomHeader = ({children, headerType, isYMoves, duration=HOOKS_DURATION_DEFAULT,
-  delay=HOOKS_DELAY_DEFAULT}
+const CustomHeader = ({children, color="var(--color-black)", headerType, isYMoves,
+  duration=HOOKS_DURATION_DEFAULT, delay=HOOKS_DELAY_DEFAULT}
    : ICustomParagraph) => {
   const ref = useRef(null);
   const isInView = useInView(ref, {once: true});
@@ -38,12 +38,12 @@ const CustomHeader = ({children, headerType, isYMoves, duration=HOOKS_DURATION_D
         hidden: {WebkitTextFillColor: "transparent", y: isYMoves === true ? 40 : 0,
           opacity: 0,
           background:
-          "linear-gradient(91.1deg, var(--color-black) -44.14%, rgba(52, 52, 52, 0) 32.72%)",
+          `linear-gradient(91.1deg, ${color} -44.14%, rgba(52, 52, 52, 0) 32.72%)`,
           backgroundClip: "text"},
         visible: {WebkitTextFillColor: "transparent", y: 0,
           opacity: 1,
           background:
-          "linear-gradient(91.1deg, var(--color-black) 97.12%, rgba(52, 52, 52, 0) 136.73%)",
+          `linear-gradient(91.1deg, ${color} 97.12%, rgba(52, 52, 52, 0) 136.73%)`,
           backgroundClip: "text"},
       }}
       initial="hidden"
--- src/types/custom-header.ts
+++ src/types/custom-header.ts
@@ -1,6 +1,7 @@
 export interface ICustomParagraph{
     children: string,
     headerType: "h1" | "h2" | "h3" | "h4",
+    color?: string,
     isYMoves: true | false,
     duration?: number,
     delay?: number,
