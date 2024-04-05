51d455b356f4abd9e3c4c03f9f1203ca6b963988
Kliruk <mazurenko.corporate@gmail.com>
2024-04-05 13:07:43 +0300

    Зробив адаптацію на планшет для header та трішки змінив адаптацію для navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,25 +17,26 @@ const Header = () => {
             <CustomHeader headerType="h2" isYmoves={true} delay={0}>
               MAKER
             </CustomHeader>
-            <p className={`paragraphMain text-left ${styles.descr}`}>Вже більше 5 років я знімаю
-              персональні та комерційні проєкти для малого та середнього бізнесу</p>
+            <p className={`paragraphTypical text-left ${styles.descr}`}>
+              Мої кадри - це поєднаннятехнічної майстерності, естетичного сприйняття та
+              глибокого розуміння емоцій, що робить кожен проєкт унікальним</p>
           </div>
         </div>
         <div className={styles.rightDoublePhoto}>
           <div className={styles.doublePhoto}>
+            <CustomImageAnim src="/photo/header/header-DRMargo-q8.1.webp"
+              uniqueClassName={styles.doubleLeftPhoto}
+              alt="Замовниця" width={236} height={355} />
             <CustomImageAnim src="/photo/header/header-DLMargo-q8.4.webp"
               uniqueClassName={styles.doubleRightPhoto}
               alt="Замовниця" width={211} height={316} />
-            <CustomImageAnim src="/photo/header/header-DRMargo-q8.1.webp"
-              uniqueClassName={styles.doubleRightPhoto}
-              alt="Замовниця" width={236} height={355} />
           </div>
           <p className={styles.backgroundTextDP}>MARG<br />ARITA</p>
         </div>
       </div>
       <div className={styles.photoAndCircleArrow}>
         <CustomImageAnim src="/photo/header/header-LMargo-q10.webp"
-          uniqueClassName={styles.doubleRightPhoto}
+          uniqueClassName={styles.mainPhoto}
           alt="Замовниця" width={662} height={400} />
         <div className={styles.circleAndArrow}>
           <NameCircle text={new Date().getFullYear()} size={132}/>
