ab140fd8c944669079eb0605d7d8cf226398e60a
AlexLeontovych <sashaleontovich2005@gmail.com>
2024-04-09 23:25:03 +0300

    Зробив PriceScroll + адаптацію

--- src/components/PriceScroll.tsx
+++ src/components/PriceScroll.tsx
@@ -1,11 +1,21 @@
 import ButtonScroll from "@/UI's/buttons/buttons-scrolls/ButtonScroll";
 import React from "react";
+import styles from "@/styles/components/price-scroll.module.css";
+
 
 const PriceScroll = () => {
   return (
-    <div>
-      <ButtonScroll size={124} widthArrow={50} widthStar={0}
-        orientation={"downward"} star={false} scrollTo={"#list-of-prices"} />
+    <div className={styles.buttonScroll}>
+      <div>
+        <p className="paragraphSmaller">Фільмую вас</p>
+        <p className="paragraphSmaller">та ваші спогади</p>
+      </div>
+      <ButtonScroll size={124} widthArrow={40} widthStar={13}
+        orientation={"upward"} star={true} scrollTo={"#list-of-prices"}/>
+      <div>
+        <p className="paragraphSmaller">Створюю креативний</p>
+        <p className="paragraphSmaller">контент</p>
+      </div>
     </div>
   );
 };
