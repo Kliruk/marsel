e99e6f2f6d1f49cb6baa12f8cb759b0826947719
AlexLeontovych <sashaleontovich2005@gmail.com>
2024-04-11 00:58:29 +0300

    Зробив footer + адаптацію

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import BackgroundText from "@/hooks/BackgroundText";
+import styles from "@/styles/components/footer.module.css";
+import CustomHeader from "@/hooks/CustomHeader";
+import ButtonScroll from "@/UI's/buttons/buttons-scrolls/ButtonScroll";
 
 const Footer = () => {
   return (
-    <div>
-
+    <div className={styles.footer}>
+      <BackgroundText size="small" uniqueClassName={styles.tkachyk}>TKACHUK</BackgroundText>
+      <CustomHeader headerType="h3" isYMoves={false}>MARSELIN.</CustomHeader>
+      <BackgroundText size="small" uniqueClassName={styles.margarita}>MARGARITA</BackgroundText>
+      <ButtonScroll uniqueClassName={styles.scrollbutton} size={100} widthArrow={30} widthStar={11}
+        orientation={"upward"} star={true} scroll={"#header"}/>
     </div>
   );
 };
