e7cbf22a96007c320c978c2da738562c95cc08f7
Kliruk <mazurenko.corporate@gmail.com>
2024-04-11 10:49:39 +0300

    зробив ```footer``` та адаптацію на нього

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,21 @@ import BackgroundText from "@/hooks/BackgroundText";
 import styles from "@/styles/components/footer.module.css";
 import CustomHeader from "@/hooks/CustomHeader";
 import ButtonScroll from "@/UI's/buttons/buttons-scrolls/ButtonScroll";
+import DevelopedByWebElefont from "@/DevelopedByWebElepfont/DevelopedByWebElefont";
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <BackgroundText size="small" uniqueClassName={styles.tkachyk}>TKACHUK</BackgroundText>
-      <CustomHeader headerType="h3" isYMoves={false}>MARSELIN.</CustomHeader>
       <BackgroundText size="small" uniqueClassName={styles.margarita}>MARGARITA</BackgroundText>
+      <div className={styles.header}>
+        <CustomHeader headerType="h3" isYMoves={false}>MARSELIN.</CustomHeader>
+      </div>
       <ButtonScroll uniqueClassName={styles.scrollbutton} size={100} widthArrow={30} widthStar={11}
         orientation={"upward"} star={true} scroll={"#header"} />
+      <div className={styles.developedBy}>
+        <DevelopedByWebElefont />
+      </div>
     </div>
   );
 };
