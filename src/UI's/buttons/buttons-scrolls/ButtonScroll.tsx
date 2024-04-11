fb74ecabd8cdc049899eeef2e3b3b8596b86f607
Kliruk <mazurenko.corporate@gmail.com>
2024-04-11 11:29:34 +0300

    adaptation fix ```PriceScroll```, додав новий пропс для компонента ```ButtonScroll``` ```uniqueClassNameArrow``` для керування svg ```arrow``` всередині кнопки

--- src/UI's/buttons/buttons-scrolls/ButtonScroll.tsx
+++ src/UI's/buttons/buttons-scrolls/ButtonScroll.tsx
@@ -13,18 +13,20 @@ import {STAR_DELAY_DEFAULT} from "@/constants/constants";
  * @param {starAnimationDelayMin} star animation delay, lowest value(min)
  * @param {uniqueClassName} unique class name in order to control css of element,
  * adaptation for example
+ * @param {uniqueClassNameArrow} uniqueClassNameArrow class name in order to control css of
+ * arrow element: adaptation, for example
  * @return {JSX.Element}
  */
 const ButtonScroll = (
     {size, widthArrow, widthStar, orientation, starAnimationDelayMin=STAR_DELAY_DEFAULT,
-      uniqueClassName, star, scrollTo}
+      uniqueClassName, star, scrollTo, uniqueClassNameArrow}
   : IButtonsScrolls) : JSX.Element => {
   return (
     <a href={scrollTo}>
       <button className={`${styles.buttonScroll} ${uniqueClassName}`}
         style={{width: size, height: size}}>
         <div className={styles.wrapper}>
-          <svg className={styles.buttonScroll_arrow} style={{transform: orientation === "upward" ? "scale(-1, -1)" : ""}} width={widthArrow} viewBox="0 0 52 76" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg className={`${styles.buttonScroll_arrow} ${uniqueClassNameArrow}`} style={{transform: orientation === "upward" ? "scale(-1, -1)" : ""}} width={widthArrow} viewBox="0 0 52 76" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 32.5793C14.5764 32.5793 25.9348 51.8452 25.9348
               75.6441M51.3242 32.5793C37.2932 32.5793 25.9348 51.8452 25.9348 75.6441M25.9348
               75.6441L25.9348 0.745361" stroke="#343434" strokeWidth="2" strokeMiterlimit="10" />
--- src/components/PriceScroll.tsx
+++ src/components/PriceScroll.tsx
@@ -5,16 +5,17 @@ import styles from "@/styles/components/price-scroll.module.css";
 
 const PriceScroll = () => {
   return (
-    <div className={styles.buttonScroll}>
-      <div>
+    <div className={styles.priceScroll}>
+      <div className={styles.leftBoxText}>
         <p className="paragraphSmaller">Фільмую вас</p>
-        <p className="paragraphSmaller">та ваші спогади</p>
+        <p className={`${styles.firstBlockSecondText} paragraphSmaller`}>та ваші спогади</p>
       </div>
-      <ButtonScroll size={124} widthArrow={40} widthStar={13}
+      <ButtonScroll uniqueClassNameArrow={styles.scrollButtonArrow}
+        uniqueClassName={styles.scrollButton} size={124} widthArrow={40} widthStar={13}
         orientation={"upward"} star={true} scrollTo={"#list-of-prices"}/>
-      <div>
+      <div className={styles.rightBoxText}>
         <p className="paragraphSmaller">Створюю креативний</p>
-        <p className="paragraphSmaller">контент</p>
+        <p className={`${styles.secondBlockSecondText} paragraphSmaller`}>контент</p>
       </div>
     </div>
   );
