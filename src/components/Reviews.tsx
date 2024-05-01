fe082c57c86b1d2c70dfa08d7f77deeb63082771
AlexLeontovych <sashaleontovich2005@gmail.com>
2024-05-01 23:30:03 +0300

    Доробив reviews (без адаптації)

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -8,6 +8,7 @@ import ButtonSwitch from "@/UI's/buttons/buttons-switch/ButtonSwitch";
 import CustomImageAnim from "@/hooks/CustomImageAnim";
 import {useAnimationControls} from "framer-motion";
 import AnimOpcY from "@/animations/AnimOpcY";
+import BackgroundText from "@/hooks/BackgroundText";
 
 const Reviews: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
@@ -41,38 +42,49 @@ const Reviews: React.FC = () => {
   const currentImage = currentReview.images[currentIndex];
 
   return (
-    <AnimOpcY
-      mainControls={reviewControls}
-      yStart={-10}
-      delay={0}
-      uniqueClassName={styles.reviews}
-    >
-      <div>
-        <div className={styles.header}>
-          <CustomHeader headerType={"h3"} isYMoves={true}>
-              Слова
-          </CustomHeader>
-          <CustomHeader headerType={"h3"} isYMoves={false}>
-              вдячностi
-          </CustomHeader>
-          <CustomHeader headerType={"h3"} isYMoves={true}>
-            {currentReview.name}
-          </CustomHeader>
-          <div className={styles.switchButtons}>
-            <ButtonSwitch widthStar={13} orientation={"left"} action={Previous} />
-            {currentIndex + 1}/{LIST_OF_REVIEWS.length}
-            <ButtonSwitch widthStar={13} orientation={"right"} action={Next} />
+    <div className={styles.reviews}>
+      <BackgroundText size="big" uniqueClassName={styles.backTextReviews} isYMoves={false}>
+        ВIДГ<br/>
+      УКИ
+      </BackgroundText>
+      <BackgroundText size="big" uniqueClassName={styles.backTextmemories} isYMoves={false}>
+        СПОГ<br/>
+        АДИ
+      </BackgroundText>
+      <div className={styles.header}>
+        <CustomHeader headerType={"h3"} isYMoves={true}>
+            Слова
+        </CustomHeader>
+        <CustomHeader headerType={"h3"} isYMoves={false}>
+            вдячностi
+        </CustomHeader>
+        <AnimOpcY
+          mainControls={reviewControls}
+          yStart={-10}
+          delay={0}
+          uniqueClassName={styles.reviews}
+        >
+          <div className={styles.animateReviews}>
+            <CustomHeader headerType={"h3"} isYMoves={true}>
+              {currentReview.name}
+            </CustomHeader>
+            <div className={styles.switchButtons}>
+              <ButtonSwitch widthStar={13} orientation={"left"} action={Previous}
+                uniqueClassName={styles.switch}/>
+              {currentIndex + 1}/{LIST_OF_REVIEWS.length}
+              <ButtonSwitch widthStar={13} orientation={"right"} action={Next}/>
+            </div>
           </div>
-        </div>
-        <div className={styles.comment}>{currentReview.comment}</div>
-        <div className={styles.photo}>
-          <CustomImageAnim uniqueClassName={styles.images} src={currentImage} alt={"Фото"}
-            width={748} height={1202}/>
-          <CustomImageAnim uniqueClassName={styles.images} src={currentImage} alt={"Фото"}
-            width={748} height={1202}/>
-        </div>
+          <div className={styles.comment}>{currentReview.comment}</div>
+          <div className={styles.photo}>
+            <CustomImageAnim uniqueClassName={styles.images} src={currentImage} alt={"Фото"}
+              width={748} height={1202}/>
+            <CustomImageAnim uniqueClassName={styles.images} src={currentImage}
+              alt={"Фото"} width={748} height={1202}/>
+          </div>
+        </AnimOpcY>
       </div>
-    </AnimOpcY>
+    </div>
   );
 };
 
